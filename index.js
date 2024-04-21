dc33869233aca8973166e739a0279f902d971ac5
ilona <gogiashvili.ilona@gmail.com>
2024-04-21 19:24:06 -0400

    commit tree 134705f20fa9adbe33810251f95cd16e21510351

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const newArr = [];
+function writeCards(names, birthday) {
+  for (let i = 0; i < names.length; i++) {
+    return newArr.push(
+      `Thank you ${names[i]}, for the wonderful ${birthday} gift!`
+    );
+  }
+  return newArr;
+}
+function countDown() {
+  let countDown = 10;
+  while (countDown >= 0) {
+    console.log(countDown--);
+  }
+}
